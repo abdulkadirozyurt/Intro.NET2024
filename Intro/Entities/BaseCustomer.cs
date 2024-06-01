a6983554801667517af1dc35be11d8dc172240c5
abdulkadirozyurt <abdulkadir.ozyurt@gmail.com>
2024-06-01 19:42:59 +0300

    inheritance and polimorphism

---
+++ Intro/Entities/BaseCustomer.cs
@@ -0,0 +1,8 @@
+﻿namespace Intro.Entities
+{
+    public class BaseCustomer
+    {
+        public string CustomerNumber { get; set; }
+        public int Id { get; set; }
+    }
+}
--- Intro/Program.cs
+++ Intro/Program.cs
@@ -54,3 +54,55 @@ foreach (var course in courses)
     Console.WriteLine("-------------------------");
 }
 
+
+
+
+Console.WriteLine("--------------------------------------------------------");
+
+
+
+
+
+
+IndividualCustomer customer1 = new IndividualCustomer();
+customer1.Id = 1;
+customer1.NationalIdentity = "12345678901";
+customer1.FirstName = "Aslı";
+customer1.LastName = "Karayiğit";
+customer1.CustomerNumber = "123456";
+
+IndividualCustomer customer2 = new IndividualCustomer();
+customer2.Id = 2;
+customer2.NationalIdentity = "12345678902";
+customer2.FirstName = "Özgür";
+customer2.LastName = "Atılgan";
+customer2.CustomerNumber = "123457";
+
+
+
+
+CorporateCustomer customer3 = new CorporateCustomer();
+customer3.Id = 3;
+customer3.CompanyName = "Kodlama.io";
+customer3.CustomerNumber = "654778";
+customer3.TaxNumber = "1254687963";
+
+CorporateCustomer customer4 = new CorporateCustomer();
+customer4.Id = 4;
+customer4.CompanyName = "ABC";
+customer4.CustomerNumber = "654779";
+customer4.TaxNumber = "1254687967";
+
+
+
+// Base sınıflar, inherit edildiği sınıfın referansını tutabilir
+// bu olaya polimorfizm denir, customers dizisi, hem bireysel hem kurumsal müşterileri içermiştir.
+BaseCustomer[] customers = { customer1, customer2, customer3, customer4 };
+
+
+
+foreach (var customer in customers)
+{
+    
+    Console.WriteLine(customer.CustomerNumber);
+}
